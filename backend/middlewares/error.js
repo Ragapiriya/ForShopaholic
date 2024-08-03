106331dcc9229f87e899a8a652ecf2a78b55b116
Ragapiriya <r2001piriya@gmail.com>
2024-08-03 16:26:42 +0530

    handling cast errors

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,3 +1,5 @@
+const ErrorHandler = require("../utils/errorHandler");
+
 module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500; //internal server error
     if(process.env.NODE_ENV == 'development'){
@@ -11,13 +13,23 @@ module.exports = (err,req,res,next)=>{
     if(process.env.NODE_ENV == 'production'){ 
 
         let message = err.message;
+        let error = {...err};
+
         if(err.name == "ValidationError"){
             message = Object.values(err.errors).map(value => value.message)
+            error = new Error(message);
         }
+        if(err.name == "CastError"){
+            message = `Resource not found ${err.path}`;;
+            error = new Error(message);
+
+            
+        }
+
         res.status(err.statusCode).json({
 
             success:false,
-            message:err.message,
+            message:error.message || "Internal Server Error",
 
         });
     }
