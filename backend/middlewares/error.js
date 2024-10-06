7a687e1bc6fb1887f658436974e2702f55e6d464
Ragapiriya <r2001piriya@gmail.com>
2024-10-06 10:51:45 +0530

    Error handling for authentication related functionalities

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,36 +1,46 @@
 const ErrorHandler = require("../utils/errorHandler");
 
-module.exports = (err,req,res,next)=>{
-    err.statusCode = err.statusCode || 500; //internal server error
-    if(process.env.NODE_ENV == 'development'){
-        res.status(err.statusCode).json({
-            success:false,
-            message:err.message,
-            stack:err.stack,
-            error:err //there will be field of data including the name of the error, message, statuscode,etc,etc
-        });
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500; //internal server error
+  if (process.env.NODE_ENV == "development") {
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+      stack: err.stack,
+      error: err, //there will be field of data including the name of the error, message, statuscode,etc,etc
+    });
+  }
+  if (process.env.NODE_ENV == "production") {
+    let message = err.message;
+    let error = { ...err };
+
+    if (err.name == "ValidationError") {
+      message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message);
     }
-    if(process.env.NODE_ENV == 'production'){ 
-
-        let message = err.message;
-        let error = {...err};
-
-        if(err.name == "ValidationError"){
-            message = Object.values(err.errors).map(value => value.message)
-            error = new ErrorHandler(message);
-        }
-        if(err.name == "CastError"){
-            message = "Resource not found" ;
-            error = new ErrorHandler(message);
-
-            
-        }
-
-        res.status(err.statusCode).json({
-
-            success:false,
-            message:error.message || "Internal Server Error",
-
-        });
+    if (err.name == "CastError") {
+      message = "Resource not found";
+      error = new ErrorHandler(message);
     }
-}
+    if (err.code == 11000) {
+      //When registering the same email again
+      let message = `Duplicate ${Object.keys(err.keyValue)} error`;
+      error = new ErrorHandler(message);
+    }
+    if (err.name == "JSONWebTokenError") {
+      //Invalid JWT token
+      let message = `JSONWebToken is invalid. Try again`;
+      error = new ErrorHandler(message);
+    }
+    if (err.name == "TokenExpiredError") {
+      //Expired JWT token
+      let message = `JSONWebToken is expired. Try again`;
+      error = new ErrorHandler(message);
+    }
+
+    res.status(err.statusCode).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
