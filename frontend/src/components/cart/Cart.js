10806ebdf9dad6c70905770d94b6ab79aade6c95
Ragapiriya <r2001piriya@gmail.com>
2024-11-21 11:50:57 +0530

    checkout steps

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -53,11 +53,11 @@ export default function Cart() {
                       </div>
 
                       <div className="col-5 col-lg-3">
-                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                        <Link to={`/product/Rs.{item.product}`}>{item.name}</Link>
                       </div>
 
                       <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                        <p id="card_item_price">${item.price}</p>
+                        <p id="card_item_price">Rs.{item.price}</p>
                       </div>
 
                       <div className="col-4 col-lg-3 mt-4 mt-lg-0">
@@ -114,7 +114,7 @@ export default function Cart() {
                 <p>
                   Est. total:{" "}
                   <span className="order-summary-values">
-                    $
+                    Rs.
                     {items.reduce(
                       (acc, item) => acc + item.quantity * item.price,
                       0
--- frontend/src/components/cart/Shipping.js
+++ frontend/src/components/cart/Shipping.js
@@ -1,8 +1,28 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { countries } from "countries-list";
 import { useDispatch, useSelector } from "react-redux";
 import { saveShippingInfo } from "../../slices/cartSlice";
 import { useNavigate } from "react-router-dom";
+import CheckoutStep from "./CheckoutStep";
+import { toast } from "react-toastify";
+export const validateShipping = (shippingInfo, navigate) => {
+  //ensure the user filled all the details before proceeding into next step
+  if (
+    !shippingInfo.address ||
+    !shippingInfo.city ||
+    !shippingInfo.province ||
+    !shippingInfo.country ||
+    !shippingInfo.phoneNo ||
+    !shippingInfo.postalCode
+  ) {
+    toast.error("Please fill the shipping information completely",
+      {
+        position:"bottom-center"
+      }
+    );
+    navigate("/shipping"); //again redirecting to the shipping page
+  }
+};
 
 export default function Shipping() {
   const { shippingInfo } = useSelector((state) => state.cartState);
@@ -30,94 +50,97 @@ export default function Shipping() {
     navigate("/order/confirm");
   };
   return (
-    <div className="row wrapper">
-      <div className="col-10 col-lg-5">
-        <form className="shadow-lg" onSubmit={submitHandler}>
-          <h1 className="mb-4">Shipping Info</h1>
-          <div className="form-group">
-            <label htmlFor="address_field">Address</label>
-            <input
-              type="text"
-              id="address_field"
-              className="form-control"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              required
-            />
-          </div>
+    <Fragment>
+      <CheckoutStep shipping={true} />
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form className="shadow-lg" onSubmit={submitHandler}>
+            <h1 className="mb-4">Shipping Info</h1>
+            <div className="form-group">
+              <label htmlFor="address_field">Address</label>
+              <input
+                type="text"
+                id="address_field"
+                className="form-control"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="city_field">City</label>
-            <input
-              type="text"
-              id="city_field"
-              className="form-control"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="postal_code_field">Province/ State</label>
-            <input
-              type="text"
-              id="postal_code_field"
-              className="form-control"
-              value={province}
-              onChange={(e) => setProvince(e.target.value)}
-              required
-            />
-          </div>
+            <div className="form-group">
+              <label htmlFor="city_field">City</label>
+              <input
+                type="text"
+                id="city_field"
+                className="form-control"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="postal_code_field">Province/ State</label>
+              <input
+                type="text"
+                id="postal_code_field"
+                className="form-control"
+                value={province}
+                onChange={(e) => setProvince(e.target.value)}
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlhtmlFor="phone_field">Phone No</label>
-            <input
-              type="phone"
-              id="phone_field"
-              className="form-control"
-              value={phoneNo}
-              onChange={(e) => setPhoneNo(e.target.value)}
-              required
-            />
-          </div>
+            <div className="form-group">
+              <label htmlhtmlFor="phone_field">Phone No</label>
+              <input
+                type="phone"
+                id="phone_field"
+                className="form-control"
+                value={phoneNo}
+                onChange={(e) => setPhoneNo(e.target.value)}
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="postal_code_field">Postal Code</label>
-            <input
-              type="number"
-              id="postal_code_field"
-              className="form-control"
-              value={postalCode}
-              onChange={(e) => setPostalCode(e.target.value)}
-              required
-            />
-          </div>
+            <div className="form-group">
+              <label htmlFor="postal_code_field">Postal Code</label>
+              <input
+                type="number"
+                id="postal_code_field"
+                className="form-control"
+                value={postalCode}
+                onChange={(e) => setPostalCode(e.target.value)}
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <label htmlFor="country_field">Country</label>
-            <select
-              id="country_field"
-              className="form-control"
-              value={country}
-              onChange={(e) => setcountry(e.target.value)}
-              required
+            <div className="form-group">
+              <label htmlFor="country_field">Country</label>
+              <select
+                id="country_field"
+                className="form-control"
+                value={country}
+                onChange={(e) => setcountry(e.target.value)}
+                required
+              >
+                {countryList.map((country, index) => (
+                  <option key={index} value={country.name}>
+                    {country.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button
+              id="shipping_btn"
+              type="submit"
+              className="btn btn-block py-3"
             >
-              {countryList.map((country, index) => (
-                <option key={index} value={country.name}>
-                  {country.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <button
-            id="shipping_btn"
-            type="submit"
-            className="btn btn-block py-3"
-          >
-            CONTINUE
-          </button>
-        </form>
+              CONTINUE
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
