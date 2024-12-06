9e13b7682d097894c33f4c27558168a1ec2f5402
Ragapiriya <r2001piriya@gmail.com>
2024-12-06 22:04:28 +0530

    live db

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,9 @@ const app = express();
 const errorMiddleware = require("./middlewares/error");
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const dotenv = require('dotenv');
-dotenv.config({path:path.join(__dirname,"config/config.env")});
-const cors = require('cors');
+const dotenv = require("dotenv");
+dotenv.config({ path: path.join(__dirname, "config/config.env") });
+const cors = require("cors");
 const products = require("./routes/product");
 const auth = require("./routes/auth");
 const order = require("./routes/order");
@@ -23,6 +23,12 @@ app.use("/api/v1/", auth); //1st URL, 2nd Route
 app.use("/api/v1/", order);
 app.use("/api/v1/", payment);
 
-
+if (process.env.NODE_ENV === "production") {
+  //frontend(build) folder access
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html "))
+  })
+}
 app.use(errorMiddleware); //last middle to be executed incase if the above middleware failed to deal with error messages.
 module.exports = app;
